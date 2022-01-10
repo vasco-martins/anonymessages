4f08d7ddfa7ede08f27308897443b14a158da5ec
vascomartins <vascomartins37@gmail.com>
2022-01-10 20:35:15 +0000

    Add adsense

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -36,12 +36,12 @@
             @csrf
             <div class="mb-3">
                 <label for="name" class="form-label">Your Name (optional)</label>
-                <input type="name" class="form-control" wire:model="name" id="name" aria-describedby="name">
+                <input type="name" class="form-control"  id="name" aria-describedby="name">
             </div>
 
             <div class="mb-3">
                 <label for="message" class="form-label">Your Message to {{ $user->name }}</label>
-                <textarea type="message" class="form-control" wire:model="message" id="name" aria-describedby="message"></textarea>
+                <textarea type="message" class="form-control" name="message" id="name" aria-describedby="message"></textarea>
             </div>
             <button type="submit" class="btn btn-primary w-100">Create</button>
         </form>
