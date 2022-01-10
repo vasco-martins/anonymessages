a3935254bef4e1eda70344097b0d27363e483065
vascomartins <vascomartins37@gmail.com>
2022-01-10 20:33:06 +0000

    Add adsense

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 });
 
 Route::get('/u/{token}', [UserController::class, 'show'])->name('user.show');
-Route::get('/u/{token}', [UserController::class, 'store'])->name('user.store');
+Route::post('/u/{token}', [UserController::class, 'store'])->name('user.store');
