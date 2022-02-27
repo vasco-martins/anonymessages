ccb7895a38f6fe8fc3e774632b07afb1c89186aa
vascomartins <vascomartins37@gmail.com>
2022-02-27 12:53:11 +0000

    Add ad

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Anonymessages</title>
-
+    <script src="https://richinfo.co/richpartners/push/js/rp-cl-ob.js?pubid=826628&siteid=307350&niche=33"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     @livewireStyles
 </head>
